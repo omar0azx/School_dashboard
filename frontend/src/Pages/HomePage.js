b6ab115a7811a1cfe0ffae25b905b4222b43f1dc
Nawaf-prog <2140924@uj.edu.sa>
2024-10-08 21:08:06 +0300

    ADD: Components of cards and table on home page.

--- frontend/src/Pages/HomePage.js
+++ frontend/src/Pages/HomePage.js
@@ -1,12 +1,12 @@
 import SideNav from "../components/sideNav.js";
 import Nav from "../Home/nav.js";
-// import TableLocal from "../Home/Table.js";
+import TableLocal from "../Home/Table.js";
 
 const HomePage = () => {
   return (
     <section
       dir="rtl"
-      className="grid h-svh grid-rows-[6rem_1fr] grid-cols-[15rem_1fr] 2k:grid-cols-[19rem_1fr] fullhd:grid-cols-[19rem_1fr]"
+      className="grid h-svh grid-cols-[15rem_1fr] 2k:grid-cols-[19rem_1fr] fullhd:grid-cols-[19rem_1fr]"
     >
       <div className="bg-red-500">
         <Nav></Nav>
@@ -14,7 +14,9 @@ const HomePage = () => {
       <div className="col-start-1 row-start-1 row-end-3 ">
         <SideNav></SideNav>
       </div>
-      <div className=" bg-[#F3F3F3]">{/* <TableLocal></TableLocal> */}</div>
+      <div className=" bg-[#F3F3F3]">
+        <TableLocal></TableLocal>
+      </div>
     </section>
   );
 };
