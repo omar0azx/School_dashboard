5351f46692afcd12dcbefcfbc1ef65ce38f1c57f
Nawaf-prog <2140924@uj.edu.sa>
2024-10-22 22:29:13 +0300

    FIX: Main Table Checkmarks

--- frontend/src/Pages/HomePage.js
+++ frontend/src/Pages/HomePage.js
@@ -6,12 +6,12 @@ const HomePage = () => {
   return (
     <section
       dir="rtl"
-      className="grid h-svh grid-cols-[15rem_1fr] 2k:grid-cols-[19rem_1fr] fullhd:grid-cols-[19rem_1fr]"
+      className="grid h-svh grid-rows-[5rem_1fr] grid-cols-[17rem_1fr] 2k:grid-cols-[19rem_1fr] fullhd:grid-cols-[19rem_1fr]"
     >
       <div className="bg-red-500">
         <Nav></Nav>
       </div>
-      <div className="col-start-1 row-start-1 row-end-3 ">
+      <div className="col-start-1 row-start-1 row-end-3 bg-black w-full">
         <SideNav></SideNav>
       </div>
       <div className=" bg-[#F3F3F3]">
