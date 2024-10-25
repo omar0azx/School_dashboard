0c2c9ffd792f9f690a7e68e1e1651f77f75c584b
Nawaf-prog <2140924@uj.edu.sa>
2024-10-25 18:29:33 +0300

    FIX: Student Table and Side sideNav

--- frontend/src/Student/StudentsTable.js
+++ frontend/src/Student/StudentsTable.js
@@ -51,10 +51,10 @@ const StudentsTable = () => {
   ];
 
   return (
-    <div className="mt-4 mx-20 bg-white px-4 pt-3 pb-4 rounded-3xl border border-gray-200 flex-1">
+    <div className="my-10 mx-20 bg-white px-4 pt-3 pb-4 rounded-3xl border border-gray-200 flex-1">
       {/* <div className="border-x border-gray-200 rounded mt-3 bg-black p-5"> */}
       {/* Add a wrapper div with max-height and overflow-y-auto */}
-      <div className="max-h-64 lg:max-h-[75vh] overflow-y-auto">
+      <div className="max-h-64 lg:max-h-[60vh] overflow-y-auto">
         <table className="w-full text-[#718EBF] bg-white">
           <thead className="border-b-2 border-gray-300">
             <tr>
