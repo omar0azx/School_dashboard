d7d0dfdb2f40ff8b63d9b24ef9fe8b9d291a76fa
Nawaf-prog <2140924@uj.edu.sa>
2024-11-02 23:47:31 +0300

    FIX: Login page

--- frontend/src/Login/Login.js
+++ frontend/src/Login/Login.js
@@ -1,10 +1,7 @@
 import React, { useState } from "react";
 import { useNavigate, Link } from "react-router-dom";
-<<<<<<< HEAD
 import { getAuth, signInWithEmailAndPassword } from "firebase/auth"; // Import Firebase methods
 import app from "../firebase.js";
-=======
->>>>>>> b9719ca8e2e315cc9c98b9451b4af6e763591d0c
 import emailIcon from "../assets/icon_email.svg";
 import lockIcon from "../assets/icon_lock.svg";
 import showPasswordIcon from "../assets/eye_closed.svg";
@@ -14,7 +11,6 @@ const Login = () => {
   const navigate = useNavigate();
   const [showPassword, setShowPassword] = useState(false);
   const [isForgotPassword, setIsForgotPassword] = useState(false);
-<<<<<<< HEAD
   const [email, setEmail] = useState(""); // State for email
   const [password, setPassword] = useState(""); // State for password
   const [error, setError] = useState(""); // State for error messages
@@ -59,13 +55,6 @@ const Login = () => {
         setError("حدث خطأ غير متوقع. يرجى المحاولة مرة أخرى لاحقًا.");
       }
     }
-=======
-
-  const handleLoginSubmit = (e) => {
-    e.preventDefault();
-    console.log("Login successful!");
-    navigate("/HomePage");
->>>>>>> b9719ca8e2e315cc9c98b9451b4af6e763591d0c
   };
 
   const handleForgotPasswordSubmit = (e) => {
@@ -88,10 +77,6 @@ const Login = () => {
           {isForgotPassword ? "إعادة تعيين كلمة المرور" : "تسجيل الدخول"}
         </h1>
 
-<<<<<<< HEAD
-=======
-        {/* Conditional rendering for welcome text */}
->>>>>>> b9719ca8e2e315cc9c98b9451b4af6e763591d0c
         {!isForgotPassword && (
           <h2
             className="text-xl font-medium mb-7"
@@ -102,10 +87,6 @@ const Login = () => {
         )}
 
         {isForgotPassword ? (
-<<<<<<< HEAD
-=======
-          // Password Reset Form
->>>>>>> b9719ca8e2e315cc9c98b9451b4af6e763591d0c
           <form
             onSubmit={handleForgotPasswordSubmit}
             className="w-full max-w-sm"
@@ -146,7 +127,6 @@ const Login = () => {
             </div>
           </form>
         ) : (
-<<<<<<< HEAD
           <form onSubmit={handleLoginSubmit} className="w-full max-w-sm">
             <div className="mb-4 relative">
               <input
@@ -154,14 +134,6 @@ const Login = () => {
                 id="username"
                 value={email} // Bind email state
                 onChange={(e) => setEmail(e.target.value.toLowerCase())} // Convert email to lowercase
-=======
-          // Login Form
-          <form onSubmit={handleLoginSubmit} className="w-full max-w-sm">
-            <div className="mb-4 relative">
-              <input
-                type="input"
-                id="username"
->>>>>>> b9719ca8e2e315cc9c98b9451b4af6e763591d0c
                 className="shadow appearance-none border rounded-2xl w-full py-2 px-4 pr-10 text-gray-700 leading-tight focus:outline focus:shadow-outline"
                 placeholder="البريد الالكتروني"
                 style={{ backgroundColor: "#9d9d9d12" }}
@@ -173,7 +145,6 @@ const Login = () => {
                 className="absolute right-3 top-2 w-5 h-5"
               />
             </div>
-<<<<<<< HEAD
             <div className="mb-4 relative">
               <input
                 type={showPassword ? "text" : "password"}
@@ -204,31 +175,6 @@ const Login = () => {
                 {error}
               </p>
             )}
-=======
-
-            <div className="mb-4 relative">
-              <input
-                type={showPassword ? "text" : "password"}
-                id="password"
-                className="shadow appearance-none border rounded-2xl w-full py-2 px-4 pr-10 text-gray-700 leading-tight focus:outline focus:shadow-outline"
-                placeholder="كلمة المرور"
-                style={{ backgroundColor: "#9d9d9d12" }}
-                dir="rtl"
-              />
-              <img
-                src={lockIcon}
-                alt="lock icon"
-                className="absolute right-3 top-2 w-5 h-5"
-              />
-              <img
-                src={showPassword ? hidePasswordIcon : showPasswordIcon}
-                alt="toggle password visibility"
-                className="absolute left-3 top-2 w-5 h-5 cursor-pointer opacity-50"
-                onClick={togglePasswordVisibility}
-              />
-            </div>
-
->>>>>>> b9719ca8e2e315cc9c98b9451b4af6e763591d0c
             <div className="mb-4 text-left">
               <button
                 type="button"
@@ -238,10 +184,6 @@ const Login = () => {
                 هل نسيت كلمة المرور؟
               </button>
             </div>
-<<<<<<< HEAD
-=======
-
->>>>>>> b9719ca8e2e315cc9c98b9451b4af6e763591d0c
             <div className="flex items-center justify-center mt-14">
               <button
                 type="submit"
@@ -254,10 +196,6 @@ const Login = () => {
                 الدخول
               </button>
             </div>
-<<<<<<< HEAD
-=======
-
->>>>>>> b9719ca8e2e315cc9c98b9451b4af6e763591d0c
             <div className="text-center mt-4">
               <Link
                 to="/SignUpPage"
@@ -274,116 +212,3 @@ const Login = () => {
 };
 
 export default Login;
-<<<<<<< HEAD
-=======
-
-//   const [username, setUsername] = useState("");
-//   const [password, setPassword] = useState("");
-//   const navigate = useNavigate();
-
-//   const handleSubmit = async (e: React.FormEvent) => {
-// e.preventDefault();
-
-// Create a FormData object and append the username and password
-//   const formData = new FormData();
-//   formData.append("username", username);
-//   formData.append("password", password);
-
-// Send POST request to FastAPI backend
-// try {
-//   const response = await fetch("http://localhost:8000/auth/token", {
-//     method: "POST",
-//     body: formData, // Send the formData object as the request body
-//     credentials: "include", // Include credentials (cookies) with the request
-//   });
-
-//   if (response.ok) {
-//     console.log("Login successful!");
-//     // Navigate to HomePage
-//     navigate("/HomePage");
-//   } else {
-//     console.error("Login failed!");
-//     // Handle login failure (e.g., show an error message)
-//   }
-// } catch (error) {
-//   console.error("Error during login:", error);
-// }
-// console.log("Email:", email);
-// console.log("Password:", password);
-// // Navigate to HomePage
-// navigate("/HomePage");
-
-//   return (
-//     <div className="flex flex-col items-center p-4">
-//       <h1 className="text-3xl font-medium mb-14">تسجيل الدخول</h1>
-//       <form onSubmit={handleSubmit} className="w-full max-w-sm">
-//         {/* Email Input */}
-//         <div className="mb-4 relative">
-//           <input
-//             type="input"
-//             id="username"
-//             value={username}
-//             onChange={(e) => setUsername(e.target.value)}
-//             className="shadow appearance-none border rounded w-full py-2 px-4 pr-10 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-//             placeholder="البريد الالكتروني"
-//             style={{ backgroundColor: "#F0EDFF" }}
-//             dir="rtl" // Set direction to right-to-left
-//           />
-//           <img
-//             src={broIcon}
-//             alt="bro icon"
-//             className="absolute right-3 top-2 w-5 h-5"
-//           />
-//         </div>
-
-//         {/* Password Input */}
-//         <div className="mb-4 relative">
-//           <input
-//             type="password"
-//             id="password"
-//             value={password}
-//             onChange={(e) => setPassword(e.target.value)}
-//             className="shadow appearance-none border rounded w-full py-2 px-4 pr-10 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-//             placeholder="كلمة المرور"
-//             style={{ backgroundColor: "#F0EDFF" }}
-//             dir="rtl" // Set direction to right-to-left
-//           />
-//           <img
-//             src={lockIcon}
-//             alt="lock icon"
-//             className="absolute right-3 top-2 w-5 h-5"
-//           />
-//         </div>
-
-//         {/* Forgot Password Link */}
-//         <div className="mb-4">
-//           <a
-//             href="/forgot-password"
-//             className="block text-left text-[#747272] text-sm"
-//           >
-//             هل نسيت كلمة المرور؟
-//           </a>
-//         </div>
-
-//         {/* Submit Button */}
-//         <div className="flex items-center justify-center mt-14">
-//           <button
-//             type="submit"
-//             className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-6 rounded"
-//             style={{ backgroundColor: "#53ADD4", borderRadius: "15px" }}
-//           >
-//             الدخول
-//           </button>
-//         </div>
-
-//         {/* Sign Up Link */}
-//         <div className="text-center mt-4">
-//           <a href="/sign-up" className="underline text-[#747272] text-sm">
-//             لاتملك حساب ؟ انشئ حساب
-//           </a>
-//         </div>
-//       </form>
-//     </div>
-//   );
-// };
->>>>>>> b9719ca8e2e315cc9c98b9451b4af6e763591d0c
