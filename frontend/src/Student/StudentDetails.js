53b8510a2673e4b7fd704e54d827ca2cb960a37b
Nawaf-prog <2140924@uj.edu.sa>
2024-11-21 21:01:44 +0300

    FIX: Download Opportunity Report Functionality

--- frontend/src/Student/StudentDetails.js
+++ frontend/src/Student/StudentDetails.js
@@ -15,6 +15,7 @@ import {
 } from "firebase/firestore"; // Import Firestore methods
 import { getStorage, ref, getDownloadURL } from "firebase/storage"; // Import Storage functions
 import { finalReportGen } from "../components/finalReportGen.js";
+import { generateReport } from "../components/generateReport.js"; // Correct path to generatePDF
 import DownloadIcon from "../assets/icon_download_file.svg";
 
 const StudentDetails = () => {
@@ -327,11 +328,23 @@ function StudentContent({ student, navigate }) {
                         {opportunity.hour || "N/A"} س
                       </td>
                       <td className="px-4 py-2">{opportunity.date || "N/A"}</td>
+                      <td className="px-4 py-3 flex justify-center items-center">
+                        <button
+                          onClick={() => generateReport(opportunity, student)} // Call generatePDF here
+                          className="shadow-lg shadow-[#23232355] bg-[#23232372] hover:bg-[#232323d2] text-white font-bold py-2 px-6 rounded-xl"
+                          style={{
+                            borderRadius: "15px",
+                            transition: "all 0.1s ease-in-out",
+                          }}
+                        >
+                          تنزيل
+                        </button>
+                      </td>
                     </tr>
                   ))
                 ) : (
                   <tr>
-                    <td colSpan="5" className="text-center py-4 text-gray-500">
+                    <td colSpan="5" className="text-center py-5 font-semibold">
                       لا توجد فرص تطوعية
                     </td>
                   </tr>
